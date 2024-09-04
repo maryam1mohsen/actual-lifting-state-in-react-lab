b76b3b516df61d9e50d5dd170095ed2ca71cf283
Maryam <maryammohsen202002861@gmail.com>
2024-09-04 10:23:18 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import IngredientList from './components/IngredientList';
+import BurgerStack from './components/BurgerStack';
+
+export const availableIngredients = [
+  { name: 'Kaiser Bun', color: 'saddlebrown' },
+  { name: 'Sesame Bun', color: 'sandybrown' },
+  { name: 'Gluten Free Bun', color: 'peru' },
+  { name: 'Lettuce Wrap', color: 'olivedrab' },
+  { name: 'Beef Patty', color: '#3F250B' },
+  { name: 'Soy Patty', color: '#3F250B' },
+  { name: 'Black Bean Patty', color: '#3F250B' },
+  { name: 'Chicken Patty', color: 'burlywood' },
+  { name: 'Lettuce', color: 'lawngreen' },
+  { name: 'Tomato', color: 'tomato' },
+  { name: 'Bacon', color: 'maroon' },
+  { name: 'Onion', color: 'lightyellow' },
+  { name: 'Cheddar Cheese', color: '#FDE18B' },
+  { name: 'Swiss Cheese', color: '#F1E1A8' },
+];
+
+const App = () => {
+  const [stack, setStack] = useState([]);
+
+  const addToBurger = (ingredient) => {
+    setStack([...stack, ingredient]);
+  };
+
+  const removeFromBurger = (index) => {
+    setStack(stack.filter((_, i) => i !== index));
+  };
+
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+        <IngredientList
+          ingredients={availableIngredients}
+          addToBurger={addToBurger}
+        />
+        <BurgerStack stack={stack} removeFromBurger={removeFromBurger} />
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,14 @@
+const IngredientList = ({ ingredients, addToBurger }) => {
+    return (
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index} style={{ backgroundColor: ingredient.color }}>
+            {ingredient.name}
+            <button onClick={() => addToBurger(ingredient)}>+</button>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  
+  export default IngredientList;  
